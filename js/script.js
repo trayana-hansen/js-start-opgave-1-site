fb81958e609a37c126659be1e4d746f84c809207
Trayana Hansen <92487819+trayana-hansen@users.noreply.github.com>
2022-08-15 14:05:54 +0200

    Dom Markup js test

---
+++ js/script.js
@@ -0,0 +1,40 @@
+
+let mainParagraph = document.getElementById("mainParagraph").innerHTML;
+console.log(mainParagraph);
+
+let legendMain = document.getElementById("legendMain").innerHTML;
+console.log(legendMain);
+
+let monthOne = document.getElementById("monthOne");
+console.log(monthOne.value);
+
+let colorOne = document.getElementById("colorOne");
+console.log(colorOne.value);
+let radioOne = document.getElementById("radioOne");
+console.log(radioOne.value);
+let radioTwo = document.getElementById("radioTwo");
+console.log(radioTwo.value);
+let radioThree = document.getElementById("radioThree");
+console.log(radioThree.value);
+let rangeOne = document.getElementById("rangeOne");
+console.log(rangeOne.value);
+let buttonOne = document.getElementById("buttonOne");
+console.log(buttonOne.value);
+let reset = document.getElementById("resetOne");
+console.log(reset.value);
+let submit = document.getElementById("submitOne");
+console.log(submit.value);
+let buttonTwo = document.getElementById("buttonTwo");
+console.log(buttonTwo.value);
+let selectOne = document.getElementById("selectOne");
+console.log(selectOne.value);
+let selectTwo = document.getElementById("selectTwo");
+console.log(selectTwo.value);
+let selectThree = document.getElementById("selectThree");
+console.log(selectThree.value);
+let selectFour = document.getElementById("selectFour");
+console.log(selectFour.value);
+let selectFive = document.getElementById("selectFive");
+console.log(selectFive.value);
+let selectSix = document.getElementById("selectSix");
+console.log(selectSix.value);
