46aea57f4ca7aa645613ef083243ff4cf21326e8
Bo Nicolajsen <squirrelworks@users.noreply.github.com>
2022-08-15 07:04:31 +0200

    initial commit

---
+++ js/variables.js
@@ -0,0 +1,16 @@
+
+const a = 1234;
+const b = "1234";
+const c = 123.4;
+const d = [1, 2, 3, 4]
+const e = { type: "Fiat", model: "500", color: "white" };
+const f = true;
+const g = null;
+const h = undefined;
+const i = function () { };
+const j = new Date();
+
+
+
+
+
